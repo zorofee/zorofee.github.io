4d2a4101754fcf23a86cfbcf2f5e74afc440c08c
zhaowanfei <zhaowanfei@xiaohongshu.com>
2023-06-07 21:28:14 +0800

    N

--- src/shaders/PBRShader.js
+++ src/shaders/PBRShader.js
@@ -274,7 +274,7 @@ void main() {
     color = mix(color, color * rmaSample.b, uOcclusion);
 
     // Convert to sRGB to display //
-    FragColor.rgb = linearToSRGB(color);//reflection;/////
+    FragColor.rgb = reflection;//linearToSRGB(color);//reflection;/////
     FragColor.a = 1.0;
 }
 `;
